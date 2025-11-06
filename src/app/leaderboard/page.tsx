d4c269755b103c1f35349bee8f2ce1eb1b46f31a
Aghostraa <ahouraazarbin@gmail.com>
2025-11-06 14:11:12 +0100

    Added a leaderboard backup before we fix the analytics api

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,10 +1,10 @@
-import LeaderboardTable from '@/components/LeaderboardTable';
+import LeaderboardTableBackup from '@/components/LeaderboardTable.graphql.backup';
 
 export default function LeaderboardPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-white">
       <main className="flex-grow py-16 px-4">
-        <LeaderboardTable />
+        <LeaderboardTableBackup />
       </main>
     </div>
   );
